f9d5bfd08bf44bd4ffe7f7a98e05ac108e708b23
Sivasankaran76 <sivasankaran243@gmail.com>
2021-12-28 10:22:44 +0530

    all files added

---
+++ src/main/java/com/springboot/devops/Week10assignmentSivasankaranApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10assignmentSivasankaranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10assignmentSivasankaranApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
